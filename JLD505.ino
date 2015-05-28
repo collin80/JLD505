c27f97fbff7a44b07b0d2300912e49570e4dbf0d
Collin Kidder <collink@kkmfg.com>
2015-05-27 23:02:34 -0400

    Increased frequency of chademo frames to be pretty reliably close to 100ms

--- JLD505.ino
+++ JLD505.ino
@@ -90,7 +90,7 @@ void timer2Int()
 {
 	timerFastCounter++;
 	timerChademoCounter++;
-	if (timerChademoCounter >= 4)
+	if (timerChademoCounter >= 3)
 	{
 		timerChademoCounter = 0;
 		if (chademo.bChademoMode  && chademo.bChademoSendRequests) chademo.bChademoRequest = 1;
